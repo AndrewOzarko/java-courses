2b04a0ef09f3cc1644e7ffeb04435a49174c91d7
Ozarko <andrewozarko@gmail.com>
2018-01-02 14:01:34 +0200

    Calculate update 1

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,15 @@
 public class Calculate {
 	public static void main(String[] arg) {
 		System.out.println("Calculate...");
+		
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		
+		System.out.println("Sum: " + (first + second));
+		System.out.println("Subtraction: " + (first - second));
+		System.out.println("Multiplication: " + (first * second));
+		System.out.println("Division: " + (first / second));
+		System.out.println("sqr first: " + (first * first));
+		System.out.println("sqr second: " + (second * second));
 	}
 }
